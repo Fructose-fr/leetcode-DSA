c1abba687ba384e1761a32960528bdf979e78c7e
Umair Ahmed <umairahmadamanat3@gmail.com>
2024-11-04 03:04:01 -0800

    Time: 0 ms (100%), Space: 10.2 MB (31.03%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int hg =0;
-        int altitude = 0;
-    for(int i=0; i<gain.size(); i++){
-        altitude += gain[i];
-        if(hg < altitude){
-            hg = altitude;
-        }
-    }    
-    return hg;
+            int g=0, t=0;
+            for(int i=0; i<gain.size(); i++)
+            {
+                g += gain[i];
+               t= max(t, g);
+                
+            }
+    return t;
     }
 };
